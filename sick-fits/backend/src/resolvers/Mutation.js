4f5b10593f9f46c2116f95e5024f09907aa8d04c
sarah4594 <sarah4594@gmail.com>
2019-06-22 11:30:35 -0500

    Change query and mutation changes form do to items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,28 @@
 const Mutation = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    // create a dog
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+  async createItem(parent, args, ctx, info) {
+    // TODO! Checked if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // title: args.title
+          // desc: args.desc
+          // same as...
+          ...args,
+        },
+      },
+      info,
+    )
+
+    return item
   },
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   // create a dog
+  //   const newDog = { name: args.name }
+  //   global.dogs.push(newDog)
+  //   return newDog
+  // },
 }
 
 module.exports = Mutation
