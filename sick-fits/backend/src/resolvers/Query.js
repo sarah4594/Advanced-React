7693fe8687dec510ad85f1a1cc39c85f0d9350f3
sarah4594 <sarah4594@gmail.com>
2019-06-22 11:43:30 -0500

    Forward all query items to database

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,12 +1,17 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
+  items: forwardTo('db'),
+
   // dogs(parent, args, ctx, info) {
   //   global.dogs = global.dogs || []
   //   return global.dogs
   // },
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items()
-    return items
-  },
+
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // },
 }
 
 module.exports = Query
