2ecb407e4ed02db9b36e234049142e15aaff1f50
sarah4594 <sarah4594@gmail.com>
2019-06-29 10:55:02 -0500

    Update item using mutations

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -16,13 +16,26 @@ const Mutation = {
 
     return item
   },
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || []
-  //   // create a dog
-  //   const newDog = { name: args.name }
-  //   global.dogs.push(newDog)
-  //   return newDog
-  // },
+
+  updateItem(parent, args, ctx, info) {
+    // first take copy of updates
+    const updates = { ...args }
+    //remove ID from updates because con't update IDs
+    delete updates.id
+    // run upadte method
+    return ctx.db.mutation.updateItem(
+      {
+        // data = what data to actually update
+        data: updates,
+        // where = tells which item to update
+        where: {
+          id: args.id,
+        },
+        // info = how updateItem knows what to return
+      },
+      info,
+    )
+  },
 }
 
 module.exports = Mutation
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require('prisma-binding')
 
 const Query = {
   items: forwardTo('db'),
+  item: forwardTo('db'),
 
   // dogs(parent, args, ctx, info) {
   //   global.dogs = global.dogs || []
