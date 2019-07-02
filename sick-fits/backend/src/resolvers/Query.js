b4ba64bc506c19c9f76bd66feb31467111312d3a
sarah4594 <sarah4594@gmail.com>
2019-07-02 14:43:21 -0500

    Add pagination using query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding')
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
 
   // dogs(parent, args, ctx, info) {
   //   global.dogs = global.dogs || []
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -2,7 +2,8 @@ import Items from '../components/Items'
 
 const Home = props => (
   <div>
-    <Items />
+    {/* Use the || 1 to fall back on if it's the first page */}
+    <Items page={parseFloat(props.query.page) || 1} />
   </div>
 )
 
