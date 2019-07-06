a2fcb2806bfc84dd92cca2954ab9848ea07b0b96
sarah4594 <sarah4594@gmail.com>
2019-07-06 10:18:50 -0500

    Create user with mutation in backend

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,8 +1,14 @@
+const cookieParser = require('cookie-parser')
+
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
 
 const server = createServer()
+
+// TODO Use exporess middleware to habdle cookies (JWT)
+server.express.use(cookieParser())
+// TODO Use express middleware to populate current user
 
 server.start(
   {
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const Mutation = {
   async createItem(parent, args, ctx, info) {
     // TODO! Checked if they are logged in
@@ -43,6 +46,33 @@ const Mutation = {
     // 2. check if they own that item or have permissions
     // 3. delete it
     return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    // Problem with authenticating when case is involved
+    args.email = args.email.toLowerCase()
+    // hash their password, can't put passwords in db
+    // SALT - makes your generation unique
+    const password = await bcrypt.hash(args.password, 10)
+    // Create user in db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    )
+    // Create JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // We set the JWT as a cookie on the repsponse
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    })
+    // Return user to the browser
+    return user
   },
 }
 
