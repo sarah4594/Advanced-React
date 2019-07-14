b5209a44b35ef08256079e2c080cacd9a67658d8
sarah4594 <sarah4594@gmail.com>
2019-07-14 18:23:49 -0500

    Create permissions page with query in frontend

---
+++ sick-fits/frontend/components/Permissions.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import PropTypes from 'prop-types'
+import Error from './ErrorMessage'
+import Table from './styles/Table'
+import SickButton from './styles/SickButton'
+
+const possiblePermissions = [
+  'ADMIN',
+  'USER',
+  'ITEMCREATE',
+  'ITEMUPDATE',
+  'ITEMDELETE',
+  'PERMISSIONUPDATE',
+]
+
+const ALL_USERS_QUERY = gql`
+  query ALL_USERS_QUERY {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`
+
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) =>
+      console.log(data) || (
+        <div>
+          <Error error={error} />
+          <div>
+            <h2>Manage Permissions</h2>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  {possiblePermissions.map(permission => (
+                    <th key={permission}>{permission}</th>
+                  ))}
+                  <th>Update</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.users.map(user => (
+                  <User user={user} key={user.id} />
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      )
+    }
+  </Query>
+)
+
+class User extends React.Component {
+  render() {
+    const user = this.props.user
+    return (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        {possiblePermissions.map(permission => (
+          <td key={user.id}>
+            <label htmlFor={`${user.id}-permission-${permission}`}>
+              <input type="checkbox" />
+            </label>
+          </td>
+        ))}
+        <td>
+          <SickButton>Update</SickButton>
+        </td>
+      </tr>
+    )
+  }
+}
+
+User.propTypes = {
+  user: PropTypes.object,
+}
+
+export default Permissions
