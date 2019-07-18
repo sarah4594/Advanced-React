3b238e6c8591b75260aa17ba66ff81ab85a63632
sarah4594 <sarah4594@gmail.com>
2019-07-18 14:58:37 -0500

    Create template for cart

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,8 +1,9 @@
-import Nav from './Nav'
 import Link from 'next/link'
 import styled from 'styled-components'
 import Router from 'next/router'
 import NProgress from 'nprogress'
+import Nav from './Nav'
+import Cart from './Cart'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -54,18 +55,18 @@ const StyledHeader = styled.header`
 
 const Header = () => (
   <StyledHeader>
-    <div className='bar'>
+    <div className="bar">
       <Logo>
-        <Link href='/'>
+        <Link href="/">
           <a>Sick Fits</a>
         </Link>
       </Logo>
       <Nav />
     </div>
-    <div className='sub-bar'>
+    <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 )
 
