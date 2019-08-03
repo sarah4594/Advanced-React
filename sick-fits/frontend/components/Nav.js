1cc1d6b7562430ed83029671e76a33029e6f366d
sarah4594 <sarah4594@gmail.com>
2019-08-03 10:08:23 -0500

    ♻ Refactor nav bar to reflect login status

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,13 +1,15 @@
-import AdminNav from './NavBar/AdminNav'
 import SignedInNav from './NavBar/SignedInNav'
 import SignedOutNav from './NavBar/SignedOutNav'
+import User from './User'
 
-const Nav = me => (
-  <>
-    <SignedInNav />
-    <SignedOutNav />
-    {/* <AdminNav /> */}
-  </>
+const Nav = () => (
+  <User>
+    {({ data }) => {
+      const me = data ? data.me : null
+      if (!me) return <SignedOutNav />
+      return <SignedInNav me={me} />
+    }}
+  </User>
 )
 
 export default Nav
--- sick-fits/frontend/components/NavBar/SignedOutNav.js
+++ sick-fits/frontend/components/NavBar/SignedOutNav.js
@@ -1,27 +1,15 @@
 import Link from 'next/link'
 import NavStyles from '../styles/NavStyles'
-import User from '../User'
 
 const SignedOutNav = () => (
-  <User>
-    {({ data }) => {
-      const me = data ? data.me : null
-      return (
-        <NavStyles>
-          {!me && (
-            <>
-              <Link href="/items">
-                <a>Shop</a>
-              </Link>
-              <Link href="/signup">
-                <a>Sign In</a>
-              </Link>
-            </>
-          )}
-        </NavStyles>
-      )
-    }}
-  </User>
+  <NavStyles>
+    <Link href="/items">
+      <a>Shop</a>
+    </Link>
+    <Link href="/signup">
+      <a>Sign In</a>
+    </Link>
+  </NavStyles>
 )
 
 export default SignedOutNav
