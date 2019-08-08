e1e988dce592231338524dee856c36176d31f497
sarah4594 <sarah4594@gmail.com>
2019-08-07 20:48:04 -0500

    ✅Add test for SingleItem

---
+++ sick-fits/frontend/__tests__/SingleItem.test.js
@@ -0,0 +1,59 @@
+import { mount } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+import wait from 'waait'
+import { MockedProvider } from 'react-apollo/test-utils'
+import SingleItem, { SINGLE_ITEM_QUERY } from '../components/SingleItem'
+import { fakeItem } from '../lib/testUtils'
+
+describe('<SingleItem />', () => {
+  const mocks = [
+    {
+      // When someone makes a request with this query and variable comb
+      request: { query: SINGLE_ITEM_QUERY, variables: { id: '123' } },
+      // Return this fake data (mocked)
+      result: {
+        data: {
+          item: fakeItem(),
+        },
+      },
+    },
+  ]
+  it('renders with proper data', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123" />
+      </MockedProvider>,
+    )
+    expect(wrapper.text()).toContain('Loading...')
+    await wait()
+    wrapper.update()
+    //console.log(wrapper.debug())
+    expect(toJSON(wrapper.find('h2'))).toMatchSnapshot()
+    expect(toJSON(wrapper.find('img'))).toMatchSnapshot()
+    expect(toJSON(wrapper.find('p'))).toMatchSnapshot()
+  })
+
+  it('errors with a not found item', async () => {
+    const mocks = [
+      {
+        // When someone makes a request with this query and variable comb
+        request: { query: SINGLE_ITEM_QUERY, variables: { id: '123' } },
+        // Return this fake data (mocked)
+        result: {
+          errors: [{ message: 'Items not found' }],
+        },
+      },
+    ]
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123" />
+      </MockedProvider>,
+    )
+    await wait()
+    wrapper.update()
+    console.log(wrapper.debug())
+    const item = wrapper.find('[data-test="graphql-error"]')
+    expect(item.text()).toContain('Items not found')
+    expect(toJSON(item)).toMatchSnapshot()
+  })
+})
