3241ebdc5e2dc12db89926b9c9a2f94447bfde0e
sarah4594 <sarah4594@gmail.com>
2019-08-11 17:01:06 -0500

    ✅Finish test for NavBar

--- sick-fits/frontend/__tests__/NavBar.test.js
+++ sick-fits/frontend/__tests__/NavBar.test.js
@@ -1,34 +1,98 @@
-import { mount, shallow } from 'enzyme'
-import toJSON from 'enzyme-to-json'
+import { mount } from 'enzyme'
 import wait from 'waait'
 import { MockedProvider } from 'react-apollo/test-utils'
 import { CURRENT_USER_QUERY } from '../components/User'
 import Nav from '../components/Nav'
-import SignedInNav from '../components/NavBar/SignedInNav'
-import SignedOutNav from '../components/NavBar/SignedOutNav'
 import { fakeADMINUser, fakeUser } from '../lib/testUtils'
+import SignedInNav from '../components/NavBar/SignedInNav'
 
-describe('<AdminNavBar />', () => {
-  const mocks = [
-    {
-      request: {
-        query: CURRENT_USER_QUERY,
-      },
-      result: {
-        data: {
-          me: fakeUser(),
-        },
+const signedOutMocks = [
+  {
+    request: {
+      query: CURRENT_USER_QUERY,
+    },
+    result: {
+      data: {
+        me: null,
       },
     },
-  ]
+  },
+]
 
-  it('renders a signed in nav bar', async () => {
+const signedInMocks = [
+  {
+    request: {
+      query: CURRENT_USER_QUERY,
+    },
+    result: {
+      data: {
+        me: fakeUser(),
+      },
+    },
+  },
+]
+
+describe('<Nav />', () => {
+  it('renders a signed out nav bar', async () => {
     const wrapper = mount(
-      <MockedProvider mocks={mocks}>
+      <MockedProvider mocks={signedOutMocks}>
         <Nav />
       </MockedProvider>,
     )
     await wait()
-    console.log(wrapper.debug())
+    wrapper.update()
+    //console.log(wrapper.debug())
+    expect(wrapper.find('SignedOutNav').exists()).toBe(true)
+  })
+})
+
+describe('<Nav />', () => {
+  it('renders a signed in nav bar', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <Nav />
+      </MockedProvider>,
+    )
+    await wait()
+    wrapper.update()
+    //console.log(wrapper.debug())
+    expect(wrapper.find('SignedInNav').exists()).toBe(true)
+  })
+})
+
+describe('<SignedInNav />', () => {
+  it('ADMIN user shows permissions', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <SignedInNav me={fakeADMINUser()} />
+      </MockedProvider>,
+    )
+    await wait()
+    wrapper.update()
+    expect(wrapper.text()).toContain('Permissions')
+  })
+})
+
+describe('<SignedInNav />', () => {
+  it('regular user does not show permissions', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <SignedInNav me={fakeUser()} />
+      </MockedProvider>,
+    )
+    await wait()
+    wrapper.update()
+    expect(wrapper.text()).not.toContain('Permissions')
+  })
+  it("shows user's account name", async () => {
+    const user = fakeUser()
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <SignedInNav me={user} />
+      </MockedProvider>,
+    )
+    await wait()
+    wrapper.update()
+    expect(wrapper.text()).toContain(`${user.name}'s Account`)
   })
 })
