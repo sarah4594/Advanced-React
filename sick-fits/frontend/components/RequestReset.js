839da725d0114773a7655236a16b5f390d83111d
sarah4594 <sarah4594@gmail.com>
2019-08-31 11:14:36 -0500

    🎣Add hooks to RequestReset

--- sick-fits/frontend/components/RequestReset.js
+++ sick-fits/frontend/components/RequestReset.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import Form from './styles/Form'
 import Error from './ErrorMessage'
+import { useFormInput } from './hooks'
 
 const REQUEST_RESET_MUTATION = gql`
   mutation REQUEST_RESET_MUTATION($email: String!) {
@@ -12,54 +13,52 @@ const REQUEST_RESET_MUTATION = gql`
   }
 `
 
-class RequestReset extends Component {
-  state = {
-    name: '',
+function RequestReset() {
+  // state = {
+  //   name: '',
+  // }
+  // saveToState = e => {
+  //   this.setState({ [e.target.name]: e.target.value })
+  // }
+
+  const name = useFormInput('')
+  const email = useFormInput('')
+
+  async function handleSubmit(e, reset) {
+    e.preventDefault()
+    await reset()
   }
 
-  saveToState = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  render() {
-    return (
-      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
-        {/* called = boolean that says whether or not mutation
+  return (
+    <Mutation
+      mutation={REQUEST_RESET_MUTATION}
+      variables={{ name: name.value, email: email.value }}
+    >
+      {/* called = boolean that says whether or not mutation
         has been called yet */}
-        {(reset, { error, loading, called }) => (
-          <Form
-            method="post"
-            data-test="form"
-            onSubmit={async e => {
-              e.preventDefault()
-              await reset()
-              this.setState({ email: '' })
-            }}
-          >
-            <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Forgot Your Password</h2>
-              <Error error={error} />
-              {!error && !loading && called && (
-                <p>Success! Check you email for a reset link</p>
-              )}
-              <label htmlFor="email">
-                Email
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  defaultValue={this.state.email}
-                  onChange={this.saveToState}
-                />
-              </label>
+      {(reset, { error, loading, called }) => (
+        <Form
+          method="post"
+          data-test="form"
+          onSubmit={e => handleSubmit(e, reset)}
+        >
+          <fieldset disabled={loading} aria-busy={loading}>
+            <h2>Forgot Your Password</h2>
+            <Error error={error} />
+            {!error && !loading && called && (
+              <p>Success! Check you email for a reset link</p>
+            )}
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" placeholder="email" {...email} />
+            </label>
 
-              <button type="submit">Reset Password</button>
-            </fieldset>
-          </Form>
-        )}
-      </Mutation>
-    )
-  }
+            <button type="submit">Reset Password</button>
+          </fieldset>
+        </Form>
+      )}
+    </Mutation>
+  )
 }
 
 export default RequestReset
